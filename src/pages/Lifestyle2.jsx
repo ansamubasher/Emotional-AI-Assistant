0e7e76e6038946db62ce47317920e74bf47523a9
ansa <147871690+ansamubasher@users.noreply.github.com>
2026-04-02 15:37:09 +0500

    Add files via upload

---
+++ src/pages/Lifestyle2.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import image from "../assets/Vector.svg"
+import "../styles/Lifestyle2.css"
+function Lifestyle2() {
+
+        const [screenTime, setScreenTime]= useState(0);
+        const [workout, setWorkout]= useState("");
+        const [steps, setSteps]= useState("");
+        const [mealsSkipped, setMealsSkipped]= useState("");
+        const [caffeinIntake, setCaffeinIntake]= useState("");
+         function handleSave(){
+                console.log("All the data entered has been saved")
+         }
+
+        return ( 
+                <div className="screen">
+                   
+                        {/* // background image */}
+                        <img
+                                src={image}
+                                alt="background"
+                                className="background"
+                        />
+                        {/* // cards */}
+                        <div className="card1">
+
+
+                        </div>
+                        <div className="card2">
+
+
+                        </div>
+
+                        <div className="card3">
+
+                        </div>
+                        <p className="ScreenTime">
+                            SCREEN TIME
+                        </p>
+                        <p className="headingC1">
+                                Please enter your total screen time
+                        </p>
+<h1>
+                                LIFESTYLE
+                        </h1>
+                        <input
+                        className ="inputField"
+                        >
+                        </input>
+                        <button className="button5">
+                                Yes
+                        </button>
+
+                        <button className="button6">
+                                No
+                        </button>
+                        <p className ="physical">
+                            PHYSICAL ACTIVITY
+                        </p>
+                        <p className="headingC2">
+                                Did you workout today?
+                        </p>
+
+                        <p className="stepsHeading">
+                                Number of Steps
+                        </p>
+                        <input className= "stepsInput">
+                        </input>
+
+
+
+                        <p className="meal">
+                            MEAL
+                        </p>
+                       <p className="meal2">
+                            Any meal skipped?
+                        </p>
+                        <button className="mealButton">
+                            YES
+                        </button>
+
+                        <button className="mealButton2">
+                            NO
+                        </button>
+                        <p className= "caffeine">
+                            Caffeine Intake
+
+                        </p>
+                        <input className= "caffeineInput">
+                        </input>
+                        <button className= "confirmButton" onClick={handleSave}>
+                                Confirm
+                        </button>
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+
+
+
+        );
+}
+
+export default Lifestyle2;
